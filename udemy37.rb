c28a11e144d57884f2f8e48931b520f228bb965f
Ricky Dam <RickyDamOttawa@gmail.com>
2019-12-02 22:57:05 -0500

    Add: udemy37 - Authenticator project implementation

---
+++ udemy37.rb
@@ -0,0 +1,37 @@
+users = [
+    {username: "ricky1", password: "password1"},
+    {username: "ricky2", password: "password2"},
+    {username: "ricky3", password: "password3"}
+]
+
+def auth_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record
+    end
+  end
+  "Incorrect credentials"
+end
+
+puts "Welcome to the authenticator"
+25.times { print "-" }
+puts
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object"
+
+attempts = 1
+while attempts < 4
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+
+  authentication = auth_user(username, password, users)
+  puts authentication
+
+  puts "Press n to quit or any other key to continue: "
+  input = gets.chomp.downcase
+  break if input == "n"
+  attempts += 1
+end
+puts "You have exceeded the number of attempts" if attempts == 4
