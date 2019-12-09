22b8bf443f25560fc7c0dc9414f0f6c924560825
Ricky Dam <RickyDamOttawa@gmail.com>
2019-12-08 21:39:02 -0500

    Update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_022423) do
+ActiveRecord::Schema.define(version: 2019_12_09_022913) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
