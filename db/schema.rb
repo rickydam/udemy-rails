254c5ca21aa36ed72d195d1085a45b4b4cc2f84d
Ricky Dam <RickyDamOttawa@gmail.com>
2019-12-15 15:50:42 -0500

    Udemy113: Add association between User & Articles and add debugger view

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_184503) do
+ActiveRecord::Schema.define(version: 2019_12_15_201432) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
