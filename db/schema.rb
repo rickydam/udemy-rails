d6a6c3a50d531b21a5047c82971da9a10cb12e78
Ricky Dam <RickyDamOttawa@gmail.com>
2019-12-24 13:20:36 -0500

    Udemy162: Add article and category association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_162428) do
+ActiveRecord::Schema.define(version: 2019_12_24_163655) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
